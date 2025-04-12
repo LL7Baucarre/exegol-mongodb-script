289f43f52ad0b686d8d3a099ae8eb8292632d198
LL7Baucarré <141127344+LL7Baucarre@users.noreply.github.com>
2025-04-12 18:07:36 +0200

    Update get-mongo.sh

--- get-mongo.sh
+++ get-mongo.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-read -p "Veuillez entrer l'adresse IP cible : " target_IP
+if [ -z "$1" ]; then
+  echo "Utilisation : $0 <adresse_IP_cible>"
+  exit 1
+fi
+
+target_IP=$1
 
 curl -O https://downloads.mongodb.com/compass/mongosh-2.3.2-linux-x64.tgz
 
